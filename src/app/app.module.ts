429fc828274f64e22672ee1e424247e621c43624
alizahidch <cmalizahid@gmail.com>
2019-03-12 09:53:01 +0500

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AdminComponent } from './admin/admin.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AdminComponent,
+    TeacherComponent,
+    StudentComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule
